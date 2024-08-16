7bb43976728b8cff0912b8c65b2ef38dc7889956
Haijing <Haijing.wang@hotmail.com>
2024-08-16 13:46:45 -0700

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import workoutRoutes from "./routes/workoutRoutes.js";
 app.use(express.json());
 app.use(cors());
 
-app.use("/workout", workoutRoutes);
+app.use("/api", workoutRoutes);
 
 
 // basic home route
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -11,35 +11,74 @@ import { v4 as uuid } from "uuid";
 //     // POST/CREATE a New 
 //     .post(workoutController.add);
 
+const workoutBuffer = fs.readFileSync("./data/workout.json");
+const workoutData = JSON.parse(workoutBuffer);
 
 router.get("/", (req, res) => {
     // res.send(200);
-    const workoutBuffer = fs.readFileSync("./data/workout.json");
-    const workoutData = JSON.parse(workoutBuffer);
     return res.json(workoutData);
 });
+
 router.get("/:date", (req, res) => {
-    // res.send(200);
-    const workoutBuffer = fs.readFileSync("./data/workout.json");
-    const workoutData = JSON.parse(workoutBuffer);
-    const filterData = workoutData.filter((currentWorkout) => {
+    const foundDate = workoutData.filter((currentWorkout) => {
         return currentWorkout.date === req.params.date;
     });
-    return res.json(filterData);
+    if (!foundDate) {
+        res.status(404).send("Compliment not found the date")
+    }
+    return res.json(foundDate);
 });
 
-router.post("/", (req, res) => {
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth() + 1; // The month index starts from 0
-    const year = date.getFullYear();
-    const currentDate = `${day}-${month}-${year}`;
+// router.post("/:date/workout", (req, res) => {
+//     const foundDate = workoutData.filter((currentWorkout) => {
+//         return currentWorkout.date === req.params.date;
+//     });
 
+//     const newWorkout = {
+//         id: uuid(),
+//         setNumber: req.body.setNumber,
+//         reps: req.body.reps,
+//         weight: req.body.weight,
+//         rest: req.body.rest,
+//         note: req.body.note
+//     }
+
+//     foundDate.workout.push(newWorkout);
+
+//     // if (!foundDate) {
+//     //     res.status(404).send("Compliment not found the id")
+//     // }
+
+//     // fs.writeFileSync("./data/videos.json", JSON.stringify(workoutData));
+//     // res.send(foundDate);
+// });
+// router.post("/:date", (req, res) => {
+//     // res.send(200);
+// const date = new Date();
+// const day = date.getDate();
+// const month = date.getMonth() + 1; // The month index starts from 0
+// const year = date.getFullYear();
+// const currentDate = `${day}-${month}-${year}`;
+//     const newData = {
+//         id: uuid(),
+//         timeStamp: new Date(),
+//         date: currentDate}
+
+//     // const workoutBuffer = fs.readFileSync("./data/workout.json");
+//     // const workoutData = JSON.parse(workoutBuffer);
+//     // const filterData = workoutData.filter((currentWorkout) => {
+//     //     return currentWorkout.date === req.params.date;
+//     // });
+//     return res.json(filterData);
+// });
+
+
+router.post("/", (req, res) => {
     const newExercise = {
         id: uuid(),
         timeStamp: new Date(),
-        date: currentDate,
-        // date: req.body.date,
+        // date: currentDate,
+        date: req.body.date,
         workout: [
             {
                 id: uuid(),
@@ -63,9 +102,64 @@ router.post("/", (req, res) => {
 })
 
 
-//     videos.push(newVideo);
-//         fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
-//         res.send(videos)
+
+// router.post("/", (req, res) => {
+//     const filterData = workoutData.filter((currentData) => {
+//         return currentData.date === req.body.date;
 //     });
+
+//     // const filterWorkout = filterData.find((currentWorkout) => {
+//     //     return currentWorkout.date === req.body.exercise;
+//     // });
+
+//     if (filterData) {
+//         console.log(filterData)
+//         const newExercise = [
+//             {
+//                 id: uuid(),
+//                 exercise: req.body.exercise,
+//                 sets: [{
+//                     id: uuid(),
+//                     setNumber: req.body.setNumber,
+//                     reps: req.body.reps,
+//                     weight: req.body.weight,
+//                     rest: req.body.rest,
+//                     note: req.body.note
+//                 }]
+//             }]
+//         // const workoutBuffer = fs.readFileSync("./data/workout.json");
+//         // const workoutData = JSON.parse(workoutBuffer);
+//         filterData.workout.push(newExercise);
+//         fs.writeFileSync("./data/workout.json", JSON.stringify(filterData));
+//         res.send(200);
+//     }
+//     // else {
+//     //     const newExercise = {
+//     //         id: uuid(),
+//     //         timeStamp: new Date(),
+//     //         // date: currentDate,
+//     //         date: req.body.date,
+//     //         workout: [
+//     //             {
+//     //                 id: uuid(),
+//     //                 exercise: req.body.exercise,
+//     //                 sets: [
+//     //                     {
+//     //                         id: uuid(),
+//     //                         setNumber: req.body.setNumber,
+//     //                         reps: req.body.reps,
+//     //                         weight: req.body.weight,
+//     //                         rest: req.body.rest,
+//     //                         note: req.body.note
+//     //                     }]
+//     //             }]
+//     //     }
+//     //     // const workoutBuffer = fs.readFileSync("./data/workout.json");
+//     //     // const workoutData = JSON.parse(workoutBuffer);
+//     //     workoutData.push(newExercise);
+//     //     fs.writeFileSync("./data/workout.json", JSON.stringify(workoutData));
+//     //     res.send(200);
+//     // }
+// })
 
 export default router;
