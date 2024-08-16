3ebc0b1980eec09f7c3a53e606dbd08257262984
Haijing <Haijing.wang@hotmail.com>
2024-08-16 16:01:19 -0700

    add addset router and /:date/workout router

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -103,68 +103,84 @@ router.post("/", (req, res) => {
     res.send(200);
 })
 
-
-
-router.post("/:date/add-exercise", (req, res) => {
+router.post("/:date/workout", (req, res) => {
     const workoutBuffer = fs.readFileSync("./data/workout.json");
     const workoutData = JSON.parse(workoutBuffer);
     const filterData = workoutData.filter((currentData) => {
         return currentData.date === req.body.date;
     });
 
-    // const filterWorkout = filterData.find((currentWorkout) => {
-    //     return currentWorkout.date === req.body.exercise;
-    // });
-
     if (filterData) {
         console.log(filterData)
-        const newExercise = [
-            {
+        const newExercise =
+        {
+            id: uuid(),
+            exercise: req.body.exercise,
+            sets: [{
                 id: uuid(),
-                exercise: req.body.exercise,
-                sets: [{
-                    id: uuid(),
-                    setNumber: req.body.setNumber,
-                    reps: req.body.reps,
-                    weight: req.body.weight,
-                    rest: req.body.rest,
-                    note: req.body.note
-                }]
+                setNumber: req.body.setNumber,
+                reps: req.body.reps,
+                weight: req.body.weight,
+                rest: req.body.rest,
+                note: req.body.note
             }]
-        // const workoutBuffer = fs.readFileSync("./data/workout.json");
-        // const workoutData = JSON.parse(workoutBuffer);
+        }
 
-        filterData.push(newExercise);
-        fs.writeFileSync("./data/workout.json", JSON.stringify(filterData));
-        // res.send(200);
+        filterData[0].workout.push(newExercise);
+
+        workoutData.map((currentWorkout, index) => {
+            if (currentWorkout.id === filterData.id) {
+                return workoutData[index] = filterData[0];
+
+            }
+        })
+        console.log(workoutData)
+        fs.writeFileSync("./data/workout.json", JSON.stringify(workoutData));
+        return res.send(200);
     }
-    //     // else {
-    //     //     const newExercise = {
-    //     //         id: uuid(),
-    //     //         timeStamp: new Date(),
-    //     //         // date: currentDate,
-    //     //         date: req.body.date,
-    //     //         workout: [
-    //     //             {
-    //     //                 id: uuid(),
-    //     //                 exercise: req.body.exercise,
-    //     //                 sets: [
-    //     //                     {
-    //     //                         id: uuid(),
-    //     //                         setNumber: req.body.setNumber,
-    //     //                         reps: req.body.reps,
-    //     //                         weight: req.body.weight,
-    //     //                         rest: req.body.rest,
-    //     //                         note: req.body.note
-    //     //                     }]
-    //     //             }]
-    //     //     }
-    //     //     // const workoutBuffer = fs.readFileSync("./data/workout.json");
-    //     //     // const workoutData = JSON.parse(workoutBuffer);
-    //     //     workoutData.push(newExercise);
-    //     //     fs.writeFileSync("./data/workout.json", JSON.stringify(workoutData));
-    //     //     res.send(200);
-    // }
 })
+
+router.post("/addset", (req, res) => {
+    const workoutBuffer = fs.readFileSync("./data/workout.json");
+    const workoutData = JSON.parse(workoutBuffer);
+    console.log(req.body.dateId)
+    console.log(req.body.exerciseId)
+    const filterWorkout = workoutData.filter((currentDate) => {
+        return currentDate.id === req.body.dateId;
+    });
+    console.log(filterWorkout)
+    const filterExercise = filterWorkout[0].workout.filter((currentExercise) => {
+        return currentExercise.id === req.body.exerciseId;
+    });
+    console.log(filterExercise)
+    if (filterWorkout) {
+        const newSet =
+        {
+            id: uuid(),
+            setNumber: req.body.setNumber,
+            reps: req.body.reps,
+            weight: req.body.weight,
+            rest: req.body.rest,
+            note: req.body.note
+        }
+
+        filterExercise[0].sets.push(newSet);
+
+        workoutData.map((currentDate, index) => {
+            if (currentDate.id === filterWorkout.id) {
+                currentDate.map((currentExercise, index1) => {
+                    if (currentExercise.id === filterExercise.id) {
+                        return workoutData[index][index1] = filterExercise[0];
+                    }
+                })
+            }
+        })
+        console.log(workoutData)
+        fs.writeFileSync("./data/workout.json", JSON.stringify(workoutData));
+        return res.send(200);
+    }
+
+})
+
 
 export default router;
